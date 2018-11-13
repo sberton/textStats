91d72e4f0822da56d25d6a74b5d31aad6b1addea
sberton <sylvain.berton.pro@gmail.com>
2018-11-13 15:09:02 +0100

    add most frequent word with 4 or 10 char

--- textstats.py
+++ textstats.py
@@ -5,10 +5,12 @@ import logging as lg
 
 sc = SparkContext()
 
-def filter_stop_words(word):
+def filter_stop_words(partition):
     from nltk.corpus import stopwords
-    english_stop_words = stopwords.words("english")
-    return word not in english_stop_words
+    english_stop_words = set(stopwords.words("english"))
+    for word in partition:
+        if word not in english_stop_words:
+            yield word
 
 def load_text(text_path):
     # Split text in words
@@ -16,7 +18,7 @@ def load_text(text_path):
     # Remove stop words ('I', 'you', 'a', 'the', ...)
     pattern = re.compile(r"^((?!(@|\\)).)*$")
 
-    vocabulary = sc.textFile(text_path)\
+    vocabulary = sc.textFile(text_path, minPartitions=4)\
         .flatMap(lambda lines: lines.lower().split())\
         .flatMap(lambda word: word.split("."))\
         .flatMap(lambda word: word.split(","))\
@@ -26,13 +28,16 @@ def load_text(text_path):
         .flatMap(lambda word: word.split("\""))\
         .filter(lambda word: word is not None and len(word) > 0)\
         .filter(lambda word : pattern.match(word))\
-        .filter(filter_stop_words)
+        .persist()
+        #.mapPartitions(filter_stop_words)
 
     #compute length of each word
-    different_words = vocabulary.distinct()
-    word_len = different_words.map(lambda word: (word,len(word)))
+    most_freq_word_4 = vocabulary.filter(lambda word: len(word)==4).map(lambda word:(word,1)).reduceByKey(lambda cnt1, cnt2: cnt1 + cnt2)
+    most_freq_word_10 = vocabulary.filter(lambda word: len(word)==10).map(lambda word:(word,1)).reduceByKey(lambda cnt1, cnt2: cnt1 + cnt2)
+    #different_words = vocabulary.distinct()
+    word_len = vocabulary.map(lambda word: (word,len(word)))
 
-    return word_len
+    return word_len,most_freq_word_4,most_freq_word_10
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -48,13 +53,18 @@ def main():
     except Warning as no_datafile:
         lg.warning(no_datafile)
     else:
-        text_len = load_text(datafile)
+        text_len,most_freq_word_4,most_freq_word_10 = load_text(datafile)
          # 10 words that get a decrease in frequency in the sequel
-        longuest_word = text_len.takeOrdered(10,  key = lambda x: -x[1])
-
+        longuest_word = text_len.takeOrdered(1,  key = lambda x: -x[1])
+        word_4 = most_freq_word_4.takeOrdered(1,lambda (word, occ): -occ)
+        word_10 = most_freq_word_10.takeOrdered(1,lambda (word, occ): -occ)
         # Print results
         for word, word_len in longuest_word:
             print("Longest word: ", word)
+        for word, word_occ in word_4:
+            print("Most frequent 4-letter word: ", word)
+        for word, word_occ in word_10:
+            print("Most frequent 10-letter word: ", word)
     finally:
         lg.info('#################### Analysis is over ######################')
     
